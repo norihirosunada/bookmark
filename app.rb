bb5de4ad3eb27ab4b64ba948a76570e37dc621cc
norihirosunada <norisuna1201@gmail.com>
2020-02-09 10:15:27 +0000

    bookmark, done

--- app.rb
+++ app.rb
@@ -5,3 +5,13 @@ require 'open-uri'
 require "sinatra/json"
 
 require './models/bookmark.rb'
+
+get '/' do 
+    @bookmarks = Bookmark.all
+    erb :index
+end
+
+post '/create' do
+    Bookmark.create(title: params[:title], url: params[:url])
+    redirect '/'
+end
